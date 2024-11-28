15e8bbcff1882e9851019ea7900d9d036212b6d3
OmKumbharde <kumbhardeom18@gmail.com>
2024-11-28 09:58:14 +0530

    Need to start with ChartJs afterwards.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import HeaderBox from "@/components/ui/HeaderBox";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
 
 const Home = () => {
+    const loggedIn = { firstName: 'Om'}
     return (
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and Manage your account and transacations efficiently."
+                    />
+
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance= {1250.35}
+                    />
+                </header>
+            </div>
+        </section>
     )
 }
 
