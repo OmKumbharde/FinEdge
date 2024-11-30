74ce4ee039c26827dc667afc5e88f602873495bd
OmKumbharde <kumbhardeom18@gmail.com>
2024-11-30 10:58:20 +0530

    FYA: Sahil

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,28 @@ import HeaderBox from "@/components/ui/HeaderBox";
 import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
 
 const Home = () => {
+    const loggedIn = { firstName: 'Om'};
     return (
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox 
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn.firstName || 'Guest'}
+                        subtext="Access and Manage your account and transactions efficiently."
+                    />
+
+                    <TotalBalanceBox 
+                    accounts={[]} 
+                    totalBanks={1} 
+                    totalCurrentBalance={1250.20} />
+
+                    
+                </header>
+            </div>
+        </section>
+
     )
 }
 
---
+++ components/ui/AnimatedCounter.tsx
@@ -0,0 +1,18 @@
+'use client'
+import React from 'react'
+import CountUp from 'react-countup'
+
+const AnimatedCounter = ({ amount }: {amount: number }) => {
+  return (
+    <div>
+      <CountUp 
+      end={amount}
+      prefix='₹'
+      decimal='.'
+      decimals={2}
+       />
+    </div>
+  )
+}
+
+export default AnimatedCounter
