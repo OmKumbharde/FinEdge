3aae50b92d3a0317b1401cb8c06bef4325df36e4
Sahil-3210 <randivesahil7@gmail.com>
2024-12-07 15:20:34 +0530

    Sidebar done carried forward to OM for Auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import HeaderBox from "@/components/ui/HeaderBox";
+import RightSidebar from "@/components/ui/RightSidebar";
 import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
 
 const Home = () => {
-    const loggedIn = { firstName: 'Om'};
+    const loggedIn = { firstName: 'Om', lastName: 'Kumbharde', email:'om@gmail.com'};
     return (
         <section className="home">
             <div className="home-content">
@@ -21,7 +22,14 @@ const Home = () => {
 
                     
                 </header>
+
+                RECENT TRANSACTIONS
             </div>
+            <RightSidebar
+                user= {loggedIn}
+                transactions={[]}
+                banks={[{ currentBalance: 124.50}, { currentBalance: 124.90}]}
+            />
         </section>
 
     )
